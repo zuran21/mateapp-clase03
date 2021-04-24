8bd9f7104e956b6c0fb340858f5c97d36ca24d3a
zuran21 <81603223+zuran21@users.noreply.github.com>
2021-04-23 19:55:01 -0500

    Create mate-ejercicio03.py

---
+++ mate-ejercicio03.py
@@ -0,0 +1,16 @@
+# concatenacion
+a = "10"
+b = "20"
+c = a+b # c es "unodos"
+print(c)
+c = a * 3 # c es "unounouno"
+print (c)
+ 
+
+
+ 
+ # operaciones con variables 
+ w = "14"
+ x = "12"
+ y = w+x
+ print (w, "hola :)", x, '=', y)
