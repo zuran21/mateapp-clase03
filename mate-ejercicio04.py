bce423e2254382a90dba126ad1326ca63742e449
zuran21 <81603223+zuran21@users.noreply.github.com>
2021-04-23 20:24:55 -0500

    Create mate-ejercicio04.py

---
+++ mate-ejercicio04.py
@@ -0,0 +1,13 @@
+#concatenacion2
+
+# operaciones con variables y texto
+
+a=14
+b=12
+c = a+b
+print(a+b)
+print (a, "hola ☺", b, '=', c)
+#print (a+ "hola"+ b+ '='+ c) - ERROR
+
+#AHORA DE CONCATENAR '''''''''' TEXTO:
+print(str(a)+ "hola"+ str(b)+ '='+ str(c))
