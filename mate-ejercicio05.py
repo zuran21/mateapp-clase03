aa5311a753534cc49849a7f3b5f35c442ec3b117
zuran21 <81603223+zuran21@users.noreply.github.com>
2021-04-23 22:35:00 -0500

    Create mate-ejercicio05.py

---
+++ mate-ejercicio05.py
@@ -0,0 +1,15 @@
+#operaciones con operadores de incremento
+a=23
+print ("a=23-->", a)
+a=a+1
+print("a=a+1-->", a)
+a+=1
+print ("a+1 -->", a)
+a*=2
+print ("a*=2 -->", a)
+a/=2
+print("a/=2 -->", a)
+a-=2
+print("a-=2-->", a)
+a**=2
+print("a**=1 -->", a)
